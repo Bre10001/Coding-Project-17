92fc6a2d28d178f55ac69a89f01236ddc0e9d30b
Breanna Burris <breanna1burris@gmail.com>
2024-11-24 21:10:36 -0500

    Setup root component with global state and routing

---
+++ tour-comparison/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"; // Import React library
+import Gallery from "./Gallery.jsx"; // Import Gallery component
+import './App.css'; // Import app CSS for styling
+
+function App() { // Define App component
+    return ( // Return header and Gallery component HTML
+        <div>
+            <h1>The Tour Comparison App</h1>
+            <Gallery />
+        </div>
+    );
+}
+
+export default App; // Export App as default component
